7f378517e284b54cfa5a1d73d70227ac649010a0
Philippe Loctaux <p@philippeloctaux.com>
2019-11-11 13:47:43 +0100

    getting path as cli argumend, opening and reading its content in a Vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate clap;
+
+use clap::{App, Arg};
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("bsq")
+    .arg(Arg::with_name("path").required(true))
+    .get_matches();
+
+    let path = matches.value_of("path").unwrap();
+    let file_data = read_file(path);
+}
+
+fn read_file(path: &str) -> std::io::Result<Vec<u8>> {
+    let mut file = File::open(path)?;
+    let mut data = Vec::new();
+
+    file.read_to_end(&mut data)?;
+
+    return Ok(data);
 }
